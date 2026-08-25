e72dea8a318cc970531d5a1b3b475fc49a24c9ad
Monica Bernasconi <bernasconimonica12@gmail.com>
2026-08-25 22:25:12 +0900

    Add levels data for TOPIK exams

---
+++ data/levels.js
@@ -0,0 +1,30 @@
+export const levels = {
+    TOPIK1: {
+        id: 'TOPIK1',
+        name: 'TOPIK 1',
+        description: 'Foundation - Basic Korean',
+        vocabularyTarget: 800,
+        grammarTarget: 30
+    },
+    TOPIK2: {
+        id: 'TOPIK2',
+        name: 'TOPIK 2',
+        description: 'Everyday Korean',
+        vocabularyTarget: 1000,
+        grammarTarget: 40
+    },
+    TOPIK3: {
+        id: 'TOPIK3',
+        name: 'TOPIK 3',
+        description: 'Intermediate Korean',
+        vocabularyTarget: 1300,
+        grammarTarget: 65
+    },
+    TOPIK4: {
+        id: 'TOPIK4',
+        name: 'TOPIK 4',
+        description: 'Advanced Korean',
+        vocabularyTarget: 1500,
+        grammarTarget: 70
+    }
+};
