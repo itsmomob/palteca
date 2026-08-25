723734b248bec1d29441817b767d90653f247c9f
Monica Bernasconi <bernasconimonica12@gmail.com>
2026-08-25 22:24:41 +0900

    Create index.js to export multiple modules

---
+++ data/index.js
@@ -0,0 +1,6 @@
+export * from './levels.js';
+export * from './grammar.js';
+export * from './vocabulary.js';
+export * from './sentences.js';
+export * from './exercises.js';
+export * from './texts.js';
