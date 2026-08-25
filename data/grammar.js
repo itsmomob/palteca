2d1464d080e85519e06f144436665fa33e194690
Monica Bernasconi <bernasconimonica12@gmail.com>
2026-08-25 22:25:36 +0900

    Add grammar data for TOPIK levels 1 to 4

---
+++ data/grammar.js
@@ -0,0 +1,59 @@
+export const grammarData = {
+    TOPIK1: [
+        {
+            id: 'g_t1_001',
+            korean: '이에요/예요',
+            meaning_en: 'to be (polite)',
+            category: 'Copula',
+            examples: ['학생이에요. (I am a student.)', '의사예요. (I am a doctor.)']
+        },
+        {
+            id: 'g_t1_002',
+            korean: '은/는',
+            meaning_en: 'topic marker',
+            category: 'Particle',
+            examples: ['저는 학생이에요. (As for me, I am a student.)', '이것은 책이에요. (This is a book.)']
+        },
+        {
+            id: 'g_t1_003',
+            korean: '아/어요',
+            meaning_en: 'present tense',
+            category: 'Verb Conjugation',
+            examples: ['학교에 가요. (I go to school.)', '밥을 먹어요. (I eat rice.)']
+        }
+    ],
+    TOPIK2: [
+        {
+            id: 'g_t2_001',
+            korean: '-아/어 본 적이 있다',
+            meaning_en: 'have experience of doing',
+            category: 'Experience',
+            examples: ['한국에 가 본 적이 있어요. (I have been to Korea.)']
+        }
+    ],
+    TOPIK3: [
+        {
+            id: 'g_t3_001',
+            korean: '-기 때문에',
+            meaning_en: 'because (reason)',
+            category: 'Cause',
+            examples: ['비가 오기 때문에 집에 있어요. (I stay home because it rains.)']
+        }
+    ],
+    TOPIK4: [
+        {
+            id: 'g_t4_001',
+            korean: '-에도 불구하고',
+            meaning_en: 'despite / in spite of',
+            category: 'Concession',
+            examples: ['어려움에도 불구하고 계속 노력했어요.', '비가 오는 데에도 불구하고 경기가 진행됐어요.']
+        },
+        {
+            id: 'g_t4_002',
+            korean: '-는 반면에',
+            meaning_en: 'whereas / on the other hand',
+            category: 'Contrast',
+            examples: ['도시는 복잡한 반면에 시골은 조용해요.', '한국어는 재미있는 반면에 어려워요.']
+        }
+    ]
+};
